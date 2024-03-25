d4770383688167401b581eed83084aa259e5f3b7
LliamMRankins <lliam.rankins@gmail.com>
2024-03-25 13:30:24 -0400

    [Trial]Changed model Structure

--- bubble_augmentation/BWtoReal.py
+++ bubble_augmentation/BWtoReal.py
@@ -12,8 +12,8 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
   validation_split=0.2,
   color_mode='grayscale',
   subset="training",
-  # seed=123,
-  shuffle=False,
+  seed=123,
+  # shuffle=False,
   image_size=(256, 256),
   batch_size=64)
 
@@ -22,8 +22,8 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   validation_split=0.2,
   color_mode='grayscale',
   subset="validation",
-  # seed=123,
-  shuffle=False,
+  seed=123,
+  # shuffle=False,
   image_size=(256, 256),
   batch_size=64)
 
@@ -43,33 +43,48 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
 
 # create model
 autoE = models.Sequential([
+  layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(256, 256, 1)),
+  #(256, 256, 32)
+  layers.MaxPooling2D((2, 2), padding='same'),
+  #(128,128,32)
+  layers.MaxPooling2D((2, 2), padding='same'),
+  layers.MaxPooling2D((2, 2), padding='same'),
+  #(32, 32, 32)
 
-    # Encoder layers
-    layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(256, 256, 1)),
-    #(256, 256, 32)
-    layers.MaxPooling2D((2, 2), padding='same'),
-    #(128,128,32)
-    layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
-    #(128, 128, 64)
-    layers.MaxPooling2D((2, 2), padding='same'),
-    #(64, 64, 64)
+  layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
+  # (32, 32, 32)
+  layers.UpSampling2D((2, 2)),
+  # (64, 64, 32)
+  layers.UpSampling2D((2, 2)),
+  layers.UpSampling2D((2, 2)),
+  layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')
+
+
+    # # Encoder layers
+    # layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(256, 256, 1)),
+    # #(256, 256, 32)
+    # layers.MaxPooling2D((2, 2), padding='same'),
+    # #(128,128,32)
+    # layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+    # #(128, 128, 64)
+    # layers.MaxPooling2D((2, 2), padding='same'),
+    # #(64, 64, 64)
     
-    # Decoder layers
-    layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
-    # (64, 64, 64)
-    layers.UpSampling2D((2, 2)),
-    # (128, 128, 64)
-    layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
-    # (128, 128, 32)
-    layers.UpSampling2D((2, 2)),
-    # (256, 256, 32)
-    layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')
-    # (256, 256, 1)
-    
+    # # Decoder layers
+    # layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+    # # (64, 64, 64)
+    # layers.UpSampling2D((2, 2)),
+    # # (128, 128, 64)
+    # layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
+    # # (128, 128, 32)
+    # layers.UpSampling2D((2, 2)),
+    # # (256, 256, 32)
+    # layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')
+    # # (256, 256, 1) 
 ])
 
 autoE.compile(loss='mean_squared_error', optimizer='adam')
 
-autoE.fit(x=train_ds, epochs=10, batch_size=64)
+autoE.fit(x=train_ds, validation_data=val_ds, epochs=10, batch_size=64)
 
 pred = autoE.predict(train_ds)
