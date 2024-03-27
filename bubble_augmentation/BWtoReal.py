3967d04ccf361c4653db716cfe240113edb64e59
LliamMRankins <lliam.rankins@gmail.com>
2024-03-27 12:52:54 -0400

    [Tweak]Editing Data generator

--- bubble_augmentation/BWtoReal.py
+++ bubble_augmentation/BWtoReal.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models, utils
+from tensorflow.keras import layers, models, utils
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -7,84 +8,99 @@ from PIL import Image
 
 tf.config.experimental.set_memory_growth(tf.config.list_physical_devices('GPU')[0], True)
 
+batch_size = 8
+train_datagen = ImageDataGenerator(rescale=1./255, data_format='channels_last')
+train_generator = train_datagen.flow_from_directory(
+    "/home/iec/Documents/bubble_project/BubbleProject/datasets",
+    target_size=(256, 256, 1),
+    batch_size=batch_size,
+    class_mode='input'
+    )
+
 train_ds = tf.keras.utils.image_dataset_from_directory(
-  "/home/iec/OneDrive/Ono Project/JAEA/Project_Files/autoencoderData",
+  "/home/iec/Documents/bubble_project/BubbleProject/datasets",
   validation_split=0.2,
   color_mode='grayscale',
   subset="training",
   seed=123,
+  label_mode=None,
   # shuffle=False,
   image_size=(256, 256),
   batch_size=64)
 
 val_ds = tf.keras.utils.image_dataset_from_directory(
-  "/home/iec/OneDrive/Ono Project/JAEA/Project_Files/autoencoderData",
+  "/home/iec/Documents/bubble_project/BubbleProject/datasets",
   validation_split=0.2,
   color_mode='grayscale',
   subset="validation",
   seed=123,
+  label_mode=None,
   # shuffle=False,
   image_size=(256, 256),
   batch_size=64)
 
-# testImg = Image.fromarray(train_ds.take(1))
-# Image.show(testImg)
-
-# print(train_ds.class_names)
-
-# Possibly find way to visualize data.
-
-#Reshaping into 2D unraveled array and normalizing data
-# X_train = X_train.reshape(X_train.shape[0], 784).astype('float32')
-# X_test = X_test.reshape(X_test.shape[0], 784).astype('float32')
-
-# X_train /= 255
-# X_test /= 255
+print(train_ds.take(2))
 
 # create model
 autoE = models.Sequential([
+  #(256, 256, 1)
   layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(256, 256, 1)),
-  #(256, 256, 32)
   layers.MaxPooling2D((2, 2), padding='same'),
   #(128,128,32)
+
+  layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
   layers.MaxPooling2D((2, 2), padding='same'),
+  #(64, 64, 32)
+
+  layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
   layers.MaxPooling2D((2, 2), padding='same'),
   #(32, 32, 32)
 
+  layers.Flatten(),
+  layers.Dense(1024, activation='relu'),
+  layers.Dense(512, activation='relu'),
+  layers.Dense(1024, activation='relu'),
+  # (1024, 1)
+
+  layers.Reshape(target_shape=(32, 32, 1)),
+  # (32, 32, 1)
+
   layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
-  # (32, 32, 32)
   layers.UpSampling2D((2, 2)),
   # (64, 64, 32)
+
+  layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
   layers.UpSampling2D((2, 2)),
+  # (128, 128, 32)
+
+  layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
   layers.UpSampling2D((2, 2)),
+  # (256, 256, 32)
+
   layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')
-
-
-    # # Encoder layers
-    # layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(256, 256, 1)),
-    # #(256, 256, 32)
-    # layers.MaxPooling2D((2, 2), padding='same'),
-    # #(128,128,32)
-    # layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
-    # #(128, 128, 64)
-    # layers.MaxPooling2D((2, 2), padding='same'),
-    # #(64, 64, 64)
-    
-    # # Decoder layers
-    # layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
-    # # (64, 64, 64)
-    # layers.UpSampling2D((2, 2)),
-    # # (128, 128, 64)
-    # layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
-    # # (128, 128, 32)
-    # layers.UpSampling2D((2, 2)),
-    # # (256, 256, 32)
-    # layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')
-    # # (256, 256, 1) 
+  # (256, 256, 1)
 ])
 
-autoE.compile(loss='mean_squared_error', optimizer='adam')
+autoE.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(learning_rate=0.01))
 
-autoE.fit(x=train_ds, validation_data=val_ds, epochs=10, batch_size=64)
+autoE.fit_generator(train_generator, train_ds, epochs=10)
 
 pred = autoE.predict(train_ds)
+
+temp = pred[0]
+
+min_val = np.min(temp)
+max_val = np.max(temp)
+
+# Rescale the values to the range 0-255
+rescaled_data = (temp - min_val) * (255.0 / (max_val - min_val))
+
+# Convert the rescaled array to integers
+rescaled_data = rescaled_data.astype(np.uint8)
+
+#print(rescaled_data)
+
+# Create an image from the rescaled array
+img = Image.fromarray(np.squeeze(rescaled_data))
+
+img.save("/home/iec/Documents/bubble_project/BubbleProject/bubble_augmentation/runs/test.jpeg")
